2e87dfcb4c8bf5d84d227b0df7282930c22ebc5f
Julien Chichignoud <julien.chichignoud@gmail.com>
2015-05-12 17:03:42 +0200

    view_tile debug function

--- js/debug.js
+++ js/debug.js
@@ -14,3 +14,24 @@ function view_memory(addr, memory) {
 
     return str;
 }
+
+function view_tile(screen, index) {
+    var tileData = screen.readTileData(index, 0b11100011);
+
+    var pixelData = new Array(8 * 8)
+    for (var line = 0; line < 8; line++) {
+        var b1 = tileData.shift();
+        var b2 = tileData.shift();
+
+        for (var pixel = 0; pixel < 8; pixel++) {
+            var mask = (1 << (7-pixel));
+            var colorValue = ((b1 & mask) >> (7-pixel)) + ((b2 & mask) >> (7-pixel))*2;
+            pixelData[line * 8 + pixel] = colorValue;
+        }
+    }
+
+    var i = 0;
+    while (pixelData.length) {
+        console.log(i++ + ' ' + pixelData.splice(0, 8).join(''));
+    }
+}

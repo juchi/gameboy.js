ace32efed43a14841cbbc87984f3090cb17e6e1b
Julien Chichignoud <julien.chichignoud@gmail.com>
2015-05-29 22:13:33 +0200

    draft channel 3

--- js/sound.js
+++ js/sound.js
@@ -5,12 +5,14 @@ var APU = function(memory) {
     var audioContext = new AudioContext();
     this.channel1 = new Channel1(this, 1, audioContext);
     this.channel2 = new Channel1(this, 2, audioContext);
+    this.channel3 = new Channel3(audioContext);
 };
 APU.prototype.update = function(clockElapsed) {
     if (this.enabled == false) return;
 
     this.channel1.update(clockElapsed);
     this.channel2.update(clockElapsed);
+    this.channel3.update(clockElapsed);
 };
 APU.prototype.setSoundFlag = function(channel, value) {
     var mask = 0xFF - (1 << (channel - 1));
@@ -85,6 +87,37 @@ APU.prototype.manageWrite = function(addr, value) {
             if (value & 0x80) {
                 this.channel2.play();
             }
+            break;
+
+        case 0xFF1A:
+            // todo
+            break;
+        case 0xFF1B:
+            this.channel3.setLength(value);
+            break;
+        case 0xFF1C:
+            // todo
+            break;
+        case 0xFF1D:
+            var frequency = this.channel3.getFrequency();
+            frequency &= 0xF00;
+            frequency |= value;
+            this.channel3.setFrequency(frequency);
+            break;
+        case 0xFF1E:
+            var frequency = this.channel3.getFrequency();
+            frequency &= 0xFF;
+            frequency |= (value & 7) << 8;
+            this.channel3.setFrequency(frequency);
+            this.channel3.lengthCheck = (value & 0x40) ? true : false;
+            if (value & 0x80) {
+                this.channel3.play();
+            }
+            break;
+        case 0xFF30:case 0xFF31:case 0xFF32:case 0xFF33:case 0xFF34:case 0xFF35:case 0xFF36:case 0xFF37:
+        case 0xFF38:case 0xFF39:case 0xFF3A:case 0xFF3B:case 0xFF3C:case 0xFF3D:case 0xFF3E:case 0xFF3F:
+            var index = addr - 0xFF30;
+            this.channel3.setWaveBufferByte(index, value);
             break;
 
         case 0xFF26:
@@ -187,10 +220,91 @@ Channel1.prototype.setLength = function(value) {
     this.soundLength = 64 - (value & 0x3F);
 };
 Channel1.prototype.setEnvelopeVolume = function(volume) {
-    this.envelopeCheck = volume ? true : false;
+    this.envelopeCheck = volume > 0 && volume < 16 ? true : false;
+    if (!this.envelopeCheck)this.stop();
     this.envelopeVolume = volume;
     this.gainNode.gain.value = this.envelopeVolume * 1/100;
-}
+};
+Channel1.prototype.disable = function() {
+    this.oscillator.disconnect();
+};
+Channel1.prototype.enable = function() {
+    this.oscillator.connect(this.gainNode);
+};
+
+var Channel3 = function(audioContext) {
+    this.playing = false;
+
+    this.soundLength = 0;
+    this.soundLengthUnit = 0x4000; // 1 / 256 second of instructions
+    this.lengthCheck = false;
+
+    this.clockLength = 0;
+
+    this.buffer = new Float32Array(32);
+
+    var gainNode = audioContext.createGain();
+    gainNode.gain.value = 1;
+    this.gainNode = gainNode;
+
+    this.baseSpeed = 65536;
+    var waveBuffer = audioContext.createBuffer(1, 32, this.baseSpeed);
+
+    var bufferSource = audioContext.createBufferSource();
+    bufferSource.buffer = waveBuffer;
+    bufferSource.loop = true;
+    bufferSource.connect(gainNode);
+    bufferSource.start();
+
+    this.audioContext = audioContext;
+    this.waveBuffer = waveBuffer;
+    this.bufferSource = bufferSource;
+
+};
+Channel3.prototype.play = function() {
+    this.playing = true;
+    this.waveBuffer.copyToChannel(this.buffer, 0, 0);
+
+    this.gainNode.connect(this.audioContext.destination);
+    this.clockLength = 0;
+};
+Channel3.prototype.stop = function() {
+    this.playing = false;
+    this.gainNode.disconnect();
+};
+Channel3.prototype.update = function(clockElapsed) {
+    if (!this.playing) return;
+
+    this.clockLength  += clockElapsed;
+
+    if (this.lengthCheck && this.clockLength > this.soundLengthUnit * this.soundLength) {
+        this.clockLength = 0;
+        this.stop();
+    }
+};
+Channel3.prototype.setFrequency = function(value) {
+    value = 65536 / (2048  - value);
+    this.bufferSource.playbackRate.value = value / this.baseSpeed;
+};
+Channel3.prototype.getFrequency = function() {
+    var freq = 2048 - 65536 / (this.bufferSource.playbackRate.value * this.baseSpeed);
+    return freq | 1;
+};
+Channel3.prototype.setLength = function(value) {
+    this.soundLength = 256 - value;
+};
+Channel3.prototype.setWaveBufferByte = function(index, value) {
+    var bufferIndex = index * 2;
+
+    this.buffer[bufferIndex]   = (value >> 4) / 8 - 1; // value in buffer is in -1 -> 1
+    this.buffer[bufferIndex+1] = (value & 0x0F) / 8 - 1;
+};
+Channel3.prototype.disable = function() {
+    this.bufferSource.disconnect();
+};
+Channel3.prototype.enable = function() {
+    this.bufferSource.connect(this.gainNode);
+};
 
 APU.registers = {
     NR10: 0xFF10,

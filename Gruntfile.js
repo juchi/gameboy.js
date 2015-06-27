d62c29181036d14652c3ebb267057ea0f4debcaa
Julien Chichignoud <julien.chichignoud@gmail.com>
2015-06-27 14:25:50 +0200

    rename js folder into src

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         concat: {
             dist: {
-                src: ['js/**/*.js'],
+                src: ['src/**/*.js'],
                 dest: 'dist/gameboy.js'
             }
         }

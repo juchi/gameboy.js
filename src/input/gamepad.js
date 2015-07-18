14b73a4a1eaaf413e0e07c6250e630d61b2f38a5
Julien Chichignoud <julien.chichignoud@gmail.com>
2015-07-18 11:11:16 +0200

    document the gamepad class

--- src/input/gamepad.js
+++ src/input/gamepad.js
@@ -2,6 +2,12 @@ var GameboyJS;
 (function (GameboyJS) {
 "use strict";
 
+// This is the default buttons mapping for the Gamepad
+// It's optimized for the XBOX pad
+//
+// Any other mapping can be provided as a constructor argument of the Gamepad object
+// An alternative mapping should be an object with keys being the indexes
+// of the gamepad buttons and values the normalized gameboy button names
 var xboxMapping = {
     0: 'UP',
     1: 'DOWN',
@@ -14,7 +20,9 @@ var xboxMapping = {
 };
 
 // Gamepad listener
-// Does the mapping between the Gamepad API and the Input class
+// Communication layer between the Gamepad API and the Input class
+// Any physical controller can be used but the mapping should be provided
+// in order to get an optimal layout of the buttons (see above)
 var Gamepad = function(mapping) {
     this.gamepad = null;
     this.state = {A:0,B:0,START:0,SELECT:0,LEFT:0,RIGHT:0,UP:0,DOWN:0};
@@ -40,6 +48,7 @@ Gamepad.prototype.init = function(onPress, onRelease) {
 };
 
 Gamepad.prototype.activatePull = function() {
+    this.deactivatePull();
     this.pullInterval = setInterval(this.pullState.bind(this), 100);
 };
 
@@ -47,12 +56,12 @@ Gamepad.prototype.deactivatePull = function() {
     clearInterval(this.pullInterval);
 };
 
+// Check the state of the current gamepad in order to detect any press/release action
 Gamepad.prototype.pullState = function() {
     for (var index in this.buttonMapping) {
         var button = this.buttonMapping[index];
         var oldState = this.state[button];
         this.state[button] = this.gamepad.buttons[index].pressed;
-
 
         if (this.state[button] == 1 && oldState == 0) {
             this.managePress(button);

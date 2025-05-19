d89960fa65cbea11364fcdea5c70666a6025f803
Julien Chichignoud <julien.chichignoud@gmail.com>
2025-05-19 20:53:53 +0200

    Rework input system so input interrupts can be fired anywhere in the frame

--- src/input/input.js
+++ src/input/input.js
@@ -13,6 +13,7 @@ var Input = function(cpu, pad, canvas) {
     this.memory = cpu.memory;
     this.P1 = 0xFF00;
     this.state = 0;
+    this.interruptQueue = [];
 
     pad.init(canvas, this.pressKey.bind(this), this.releaseKey.bind(this));
 };
@@ -29,9 +30,7 @@ Input.keys = {
 };
 
 Input.prototype.pressKey = function(key) {
-    this.state |= Input.keys[key];
-
-    this.cpu.requestInterrupt(GameboyJS.CPU.INTERRUPTS.HILO);
+    this.delayInterrupt(key);
 };
 
 Input.prototype.releaseKey = function(key) {
@@ -39,14 +38,34 @@ Input.prototype.releaseKey = function(key) {
     this.state &= mask;
 };
 
+// do not send the interrupt right away, due to the way javascript works :
+// the key event fires when no other code is running, meaning when the frame()
+// in the GPU has finished rendering. This means the interrupt will always run
+// at LY = 144, which prevents the game to generate entropy for the key press actions
+//
+// the event is stored in a queue which is processed the next time the LY register is
+// at the randomly determined value
+Input.prototype.delayInterrupt = function(key) {
+    let ly = (Math.random() * 153) | 0;
+    this.interruptQueue.push({ly: ly, key: key});
+};
+
 Input.prototype.update = function() {
+    if (this.interruptQueue.length > 0) { // check for interrupt to fire
+        if (this.interruptQueue[0].ly === this.memory.rb(this.cpu.gpu.LY)) {
+            let v = this.interruptQueue.shift();
+            this.state |= Input.keys[v.key];
+            this.cpu.requestInterrupt(GameboyJS.CPU.INTERRUPTS.HILO);
+        }
+    }
+
     var value = this.memory.rb(this.P1);
     value = ((~value) & 0x30); // invert the value so 1 means 'active'
     if (value & 0x10) { // direction keys listened
         value |= (this.state & 0x0F);
     } else if (value & 0x20) { // action keys listened
         value |= ((this.state & 0xF0) >> 4);
-    } else if ((value & 0x30) == 0) { // no keys listened
+    } else if ((value & 0x30) === 0) { // no keys listened
         value &= 0xF0;
     }
 
